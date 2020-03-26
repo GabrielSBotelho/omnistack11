3285a526e2943a03e8654a010f0751f9c59c9c64
Gabriel Botelho <gabrielsousa484@outlook.com>
2020-03-25 22:01:57 -0300

    Front-end do app

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,9 +10,10 @@ const cors = require('cors');
 const app = express();
 
 // informa para o express que está usando json para o corpo das requisições
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors());
+
 /* 
     - pega a rota desejada, no caso '/' pega a rota raiz.
     - () sempre recebe dois parametros: request e response 
